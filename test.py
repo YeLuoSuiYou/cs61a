6445e3391039c6bd69f340dfcae28f34d8e976e8
jason_ye <202230283325@email.scut.edu.cn>
2023-01-29 16:19:10 +0800

    gitignore

--- test.py
+++ test.py
@@ -1,28 +1,6 @@
-def split(n):
-    return n // 10, n % 10
+def fib(n):
+    if n == 0: return 0
+    elif n == 1: return 1
+    else: return fib(n-1) + fib(n-2)
 
-
-def sum_digits(n):
-
-    if n < 10:
-        return n
-    else:
-        all_but_last, last = split(n)
-    return sum_digits(all_but_last) + last
-
-
-def luhn_sum(n):
-    if n < 10:
-        return n
-    else:
-        all_but_last, last = split(n)
-        return luhn_sum_double(all_but_last) + last
-
-
-def luhn_sum_double(n):
-    all_but_last, last = split(n)
-    luhn_digit = sum_digits(2 * last)
-    if n < 10:
-        return luhn_digit
-    else:
-        return
+print(fib(30))
