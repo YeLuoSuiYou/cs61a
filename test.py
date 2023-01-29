79b9e737954db85dd905dcba01abd353422477b0
jason_ye <202230283325@email.scut.edu.cn>
2023-01-29 18:27:47 +0800

    updae

--- test.py
+++ test.py
@@ -1,6 +1,14 @@
-def fib(n):
-    if n == 0: return 0
-    elif n == 1: return 1
-    else: return fib(n-1) + fib(n-2)
+def count_partition(n, m):
+    if n == 0:
+        return 1
+    elif n < 0:
+        return 0
+    elif m == 0:
+        return 0
+    else:
+        with_m = count_partition(n-m, m)
+        without_m = count_partition(n, m-1)
+        return with_m + without_m
 
-print(fib(30))
+
+print(count_partition(5, 3))
